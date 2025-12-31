84df8f0249b34fb7f6e18526910dc73634f09a73
hglopes89 <152782155+hglopes89@users.noreply.github.com>
2025-12-31 16:20:44 +0000

    wip: analise de um nao metodo hierarquico

--- 3.Cluster.R
+++ 3.Cluster.R
@@ -1,3 +1,11 @@
+library(factoextra)
+library(fpc)
+library(cluster)
+library(clusterCrit)
+library(aricode)
+
+
+
 load("AEDPL.RData") 
 
 summary(AEDPL)
@@ -50,9 +58,7 @@ rect.hclust(hc_ward, 4, border="blue")
 # visualmente podemos ver que existem 3 cortes possiveis interessantes
 
 # analise de clusters 
-library(fpc)
-library(factoextra)
-library(cluster);
+
 
 cluster2 <- cutree(hc_ward, 2)
 calinhara(aedl_s, cluster2)
@@ -77,9 +83,81 @@ k_escolhido <- 3
 # a aplicação de um método não hierárquico (k-means);
 set.seed(123)
 
-library(factoextra); 
 
 fviz_nbclust(aedl_s, kmeans, method = "wss")
+# a analise demonstra devem ser utilizados 2 a 3 clusters
+# porque 
 fviz_nbclust(aedl_s, kmeans, method = "silhouette")
-fviz_nbclust(aedl_s, kmeans, method = "gap_stat")
+# o valor maximo utilizado k_means com a silhouette é 3 clusters 
+
+# vai-se analisar qual o melhor numero de clusters entre 2 e 3 
+# para isso vai-se olhar para as dispersões: intra-clusters, inter-cluster e totais
+
+analise_k_cluster <- function(k){
+  set.seed(1234)
+  km <- kmeans(aedl_s, centers = k)
+  print(paste("Análise para k =", k))
+  #print(paste("Centroides das variaveis estandardizadas:", toString(km$centers)))
+  # soma total dos quadrados
+  print(paste("Soma total dos quadrados:", km$totss))
+  ratio <- km$betweenss / km$totss
+  print(paste("Rácio de Separação ", round(ratio, 4)))
+  # dispersao intra clusters
+  print(paste("Dispersao intra clusters:", toString(km$withinss)))
+  # total da dispersao intra clusters
+  print(paste("Total da dispersao intra clusters:", km$tot.withinss))
+  # dispersao inter clusters
+  print(paste("Dispersao inter clusters:", km$betweenss))
+  # dimensao dos clusters
+  print(paste("Dimensao dos clusters:", toString(km$size)))
+  
+  return(km) 
+  
+  
+}
+
+km2_cluster <- analise_k_cluster(2)$cluster
+km3_cluster <- analise_k_cluster(3)$cluster
+# o k=3 apresenta um ratio melhor e uma dispersao intra clusters menor <- rever
+
+calinhara(aedl_s, km2_cluster)
+# 401.4296 é ao valor do calinhara para k=2, que corresponde à separação entre os clusters
+calinhara(aedl_s, km3_cluster)
+# 381.6143 é o valor do calinhara para k=3, que corresponde à separação entre os clusters
+# o k=2 apresenta um valor melhor no calinhara
+
+
+fviz_silhouette(silhouette(km2_cluster, dist(aedl_s)))
+# cluster| size |ave.sil.width
+# 1      |363   |0.56
+# 2      |207   |0.17
+# average silhouette width: 0.42
+
+fviz_silhouette(silhouette(km3_cluster, dist(aedl_s)))
+#cluster| size a| av.sil.width
+#1      |346    |0.51
+#2      |62     |0.28
+#3      |162    |0.27
+# average silhouette width: 0.42
+
+
+# da analise feita pudemos verificar que o k=2 apresenta melhores resultados
+# tanto no calinhara como na silhouette
+# definimos "melhor calinhara" como o numero de clusters que apresenta maior separação entre os clusters
+# definimos "melhor silhouette" como o numero de clusters que apresenta maior coesão dentro dos clusters ou seja, os individuos estão melhor agrupados))
+
+fviz_cluster(kmeans(aedl_s, centers = 2), aedl_s, main=" ", ellipse.type="norm")
+fviz_cluster(kmeans(aedl_s, centers = 3), aedl_s, main=" ", ellipse.type="norm")
+
+k_escolhido <- 3
+
+# ------------------------------------------------------
+# a comparação dos grupos obtidos com os grupos definidos por cada uma das duas variáveis
+# qualitativas utilizadas na primeira questão.
+# Variaveis escolhidas:
+# Nominal: Posicao
+# Ordinal: FaixaEtaria
+
+
+
 
